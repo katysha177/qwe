ba38ac0e5295bfbdb6be8fa4892b789cb85d3072
katysha177 <65913619+katysha177@users.noreply.github.com>
2020-05-28 15:51:01 +0300

    ivvv

--- xyi.js
+++ xyi.js
@@ -11,21 +11,21 @@ const per3 = "petyx"
 const per4 = "evgeniy"
 const result5 = per3+per4;
 console. log (result5)
-const per5 = "true";
-const per6 = "false";
+const per5 = true;
+const per6 = false;
 const result6 = per5 && per6;
 console.log (result6)
-const per7 = "true";
-const per8 = "false";
+const per7 = true;
+const per8 = false;
 const result7 = per7 || per8;
 console.log (result7)
 const per9 = "ytka";
 const per10 = "petyx";
 const result8 = per9 === per10;
 const result9 = per9 !== per10;
-console.log = (result8,result9)
+console.log  (result8,result9)
 const per11 = "kyriza";
 const per12 = "kyriza";
 const result10 = per11===per12;
 const result11 = per11!==per12;
-console.log = (result10,result11)
+console.log (result10,result11)

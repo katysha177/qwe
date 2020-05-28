e6f1a9027bf984af0ce7987df6a99b8b2dd9bac4
katysha177 <65913619+katysha177@users.noreply.github.com>
2020-05-28 09:35:34 +0300

    (1)

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const per = “hello world”
 console.log(per)
 const her = "5"
 const her2 = "4"
-onst result = her + her2
+const result = her + her2
 const result2 = her * her2
 const result3 = her2 - her
 const result4 = her2 / her2

17c9e1a200b42f4692cce0dccf5fbd1df7b35cb8
katysha177 <65913619+katysha177@users.noreply.github.com>
2020-05-28 10:43:42 +0300

    fuck

--- xyi.js
+++ xyi.js
@@ -1,9 +1,9 @@
-const per = “hello world”
-console.log(her)
-const her = "5"
-const her2 = "4"
-const result = her + her2
-const result2 = her * her2
-const result3 = her2 - her
-const result4 = her2 / her2
-console.log(result,result2,result3,result4)
+const per = hello world;
+console.log(per);
+const per1 = "5";
+const per2 = "4";
+const result = per1 + per2;
+const result2 = per1 * per2;
+const result3 = per1 - per2;
+const result4 = per1 / per2;
+console.log(result,result2,result3,result4);

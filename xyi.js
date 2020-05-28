8d442cdd002bce9419cc4cc8caecb3c93328280f
katysha177 <65913619+katysha177@users.noreply.github.com>
2020-05-28 09:53:55 +0300

    initial commit

--- xyi.js
+++ xyi.js
@@ -1,8 +1,8 @@
 const per = “hello world”
-console.log(per)
+console.log(her)
 const her = "5"
 const her2 = "4"
-onst result = her + her2
+const result = her + her2
 const result2 = her * her2
 const result3 = her2 - her
 const result4 = her2 / her2

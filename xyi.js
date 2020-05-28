f64bd0c24f039ebd7ddc8ef1fd842075fdec6843
katysha177 <65913619+katysha177@users.noreply.github.com>
2020-05-28 11:09:14 +0300

    posl1

--- xyi.js
+++ xyi.js
@@ -7,3 +7,7 @@ const result2 = per1 * per2;
 const result3 = per1 - per2;
 const result4 = per1 / per2;
 console.log(result,result2,result3,result4)
+const per3 = "petyx"
+const per4 = "evgeniy"
+const result5 = per3+per4;
+console. log (result5)
